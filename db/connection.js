5b7e62e1105e2085ab6bd7088682f1831e2639ff
himanshu1365 <himanshu1365.cse16@chitkara.edu.in>
2019-11-28 13:21:35 +0530

    changed conection file

--- db/connection.js
+++ db/connection.js
@@ -2,8 +2,8 @@ const {MONGO_HOSTNAME,MONGO_PORT, MONGO_DB} = require('./keys')
 
 const mongoose = require('mongoose')
 const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
-
-mongoose.connect(url,{useNewUrlParser: true, useCreateIndex: true})
+const mlabURL = 'mongodb://admin:admin0018@ds349618.mlab.com:49618/smart-chat'
+mongoose.connect(mlabURL,{useNewUrlParser: true, useCreateIndex: true})
 
 mongoose.connection.once('open',()=>{
     console.log('Mongoose Connection is Successful')
