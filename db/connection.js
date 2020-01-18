195c4ee6756f00421600bc876d61128368421b1b
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-01-18 12:16:06 +0530

    changed mongo connection

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,8 @@
-const {MONGO_HOSTNAME,MONGO_PORT, MONGO_DB} = require('./keys')
+const {MONGO_DB_USER,MONGO_DB_PASSWORD,MONGO_DB} = require('./keys')
 
 const mongoose = require('mongoose')
 const Grid = require('gridfs-stream')
-const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
-
+const url = `mongodb://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@ds349618.mlab.com:49618/${MONGO_DB}`
 
 mongoose.connect(url,{useNewUrlParser: true, useCreateIndex: true},(err)=>{
     if(err) {
