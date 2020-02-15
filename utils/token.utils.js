33e3faa74041b78d962f9a50be51e604e9b1409a
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-02-15 09:20:23 +0530

    changed jwt sign in method

--- utils/token.utils.js
+++ utils/token.utils.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken')
+let privateKey = "vkjgd234vbd"
 
 let tokenUtil = {
     createJWTToken : (id)=>{
-        var token = jwt.sign({ id, expiresIn: '24h' }, new Buffer(process.env.SECRET, 'base64'));
+        var token = jwt.sign({ id, expiresIn: '24h' }, privateKey, {algorithm: 'RS256'});
         return token
     },
 
